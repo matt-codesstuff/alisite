5edf15c492817ef0f9b4d0f3304be134506281b4
matt-codesstuff <mattmatt.c0d35@gmail.com>
2023-06-27 15:28:27 +0200

    added whitenoise middleware

--- alisite/settings.py
+++ alisite/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['aliskitchen-67f842b12558.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -64,8 +64,9 @@ CKEDITOR_CONFIGS = {
    },
 }
 
-MIDDLEWARE = [
+MIDDLEWARE = [  
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -142,6 +143,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 MEDIA_ROOT = ''
 MEDIA_URL = ''
