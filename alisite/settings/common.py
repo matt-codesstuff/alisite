66ebd80d2f539966d53f0266beef2881076f473c
--global <mattmattc0d35@gmail.com>
2023-11-14 18:21:19 +0100

    some more security changes

--- alisite/settings/common.py
+++ alisite/settings/common.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'asdfsdagadfsgadfhpip'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 
